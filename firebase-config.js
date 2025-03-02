448823933608e2d430b6fe26ad9e2cd3fd2827fb
KGALPHAa <royalealphaz@gmail.com>
2025-03-02 14:49:04 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBJsRhSpnnXvM3jwLJxWQf0jzQwIEDVx_8",
+  authDomain: "crazytime-2d968.firebaseapp.com",
+  projectId: "crazytime-2d968",
+  storageBucket: "crazytime-2d968.firebasestorage.app",
+  messagingSenderId: "277085648275",
+  appId: "1:277085648275:web:0e7d8ff6e9299bc9a3a466",
+  measurementId: "G-BLDYKH8E8R"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
